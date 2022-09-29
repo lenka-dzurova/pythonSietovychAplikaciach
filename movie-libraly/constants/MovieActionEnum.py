8aa667031c83b370898ae889493fbad44403ed54
Lenka Dzurova <dzurovalenka551@gmail.com>
2022-09-29 15:44:48 +0200

    update main

---
+++ movie-libraly/constants/MovieActionEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class MenuActionEnum:
+    ADD_MOVIE = '1'
+    REMOVE_MOVIE = '2'
+    SHOW_LIBRALY = '3'
+    QUIT = 'q'
--- movie-libraly/main.py
+++ movie-libraly/main.py
@@ -1,5 +1,9 @@
+from tkinter import Menu
+
+from view.MenuView import MenuView
 from services.MovieLibralyService import MovieLibralyService
 from models.MovieModel import MovieModel
+from constants.MovieActionEnum import MenuActionEnum
 she_hulk = MovieModel('She-Hulk: Neuveriteľná právnička ', 
                     'V seriáli She-Hulk: Neuveriteľná právnička od štúdia Marvel musí Jennifer Waltersová - právnička zaoberajúca sa právnymi prípadmi spojenými so superľuďmi - žiť komplikovaným životom single ženy po tridsiatke, ktorá je zhodou náhod tiež zelená, dvojmetrová a supersilná obryňa',
                     2022,
@@ -15,4 +19,19 @@ movie_libraly.to_string()
 movie_libraly.removeModel(0)
 movie_libraly.to_string()
 
+
+menu_view = MenuView()
+
+match menu_view.menu:
+            case MenuActionEnum.ADD_MOVIE:
+                movie_libraly.addMovie(menu_view.add_movie())
+                pass
+            case MenuActionEnum.REMOVE_MOVIE:
+                pass
+            case MenuActionEnum.SHOW_LIBRALY:
+                movie_libraly.to_string()
+                pass
+            case MenuActionEnum.QUIT:
+                pass
+        
 #print(she_hulk.to_string())
