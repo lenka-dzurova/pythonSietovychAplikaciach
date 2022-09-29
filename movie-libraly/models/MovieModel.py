58c636d574949366ab5364156779dbd6bee1f7ae
Lenka Dzurova <dzurovalenka551@gmail.com>
2022-09-29 15:06:50 +0200

    movie model created

---
+++ movie-libraly/models/MovieModel.py
@@ -0,0 +1,21 @@
+from array import array
+from ast import Num
+from multiprocessing import set_forkserver_preload
+from turtle import st
+
+
+class MovieModel:
+    def __init__(self, paTitle:str, paDescription:str, paYear:Num, paGenre:array, paRating:Num) -> None:
+        self.title:str = paTitle
+        self.description:str = paDescription
+        self.year:Num = paYear
+        self.genre:array = paGenre
+        self.rating:Num = paRating
+        pass
+
+    def genre_to_string(self):
+        return '/'.join(self.genre)
+        pass
+
+    def to_string(self):
+        return f'Title: {self.title:10} | Description: {self.description:20} | Year: {self.year} | Genre: {self.genre_to_string} | Rating: {self.rating}'
